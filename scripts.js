e37122f516063fbb2c93030e6d4a2e29a719092a
Manuel <manuel.serrano943@gmail.com>
2020-11-16 13:29:19 +0100

    fixed bug when put different operators before clicking result

--- scripts.js
+++ scripts.js
@@ -23,25 +23,21 @@ function display(number) {
 
 for (let i = 0; i < operators.length; i++) {
     operators[i].addEventListener("click", function (index) {
-        console.log("operador: "+operatorSelected);
-        console.log("numero 1: "+n1);
-        console.log("numero 2: "+n2);
         if (operatorSelected != undefined) {
             if (n2 == undefined || isNaN(n2) && n1 == undefined || isNaN(n1)) {
                 
             } else {
-                n2 = Number.parseFloat(screen.textContent);
-            }
-
-            if (typeof n1 == "number" && typeof n2 == "number") {
-                console.log("numero 1: "+n1);
-                operate(operatorSelected, n1, n2);
-                operatorSelected = undefined;
+                if(screen.textContent != "") {
+                    n2 = Number.parseFloat(screen.textContent);
+                    if (typeof n1 == "number" && typeof n2 == "number") {
+                        operate(operatorSelected, n1, n2);
+                        operatorSelected = undefined;
+                    }
+                }
             }
 
         }
         if (screen.textContent != "") {
-            console.log("NUMERO 1 COGE CONTENIDO "+n1);
             n1 = Number.parseFloat(screen.textContent);
             numberSelected = "";
         }
@@ -59,12 +55,11 @@ equal.addEventListener("click", function () {
     }
 
     if (typeof n1 == "number" && typeof n2 == "number") {
-        console.log(n2);
         operate(operatorSelected, n1, n2);
         operatorSelected = undefined;
         numberSelected = "";
-    } else {
-        console.error("NUMERO 1 NO ES UN NUMERO: " + n1 + " O NUMERO 2: " + n2);
+        n1 = "";
+        n2 = "";
     }
 
 
